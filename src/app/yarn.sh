b283b0eb21d11f65b4c1cd679e5f3d8d2c213a51
Kamontat Chantrachirathumrong <developer@kamontat.net>
2020-09-04 21:47:36 +0700

    remove unnessary condition

--- src/app/yarn.sh
+++ src/app/yarn.sh
@@ -3,9 +3,7 @@
 __myzs_initial "$0"
 
 export NODE_LTS_PATH="/usr/local/opt/node@12/bin"
-if __myzs_is_folder_exist "$NODE_LTS_PATH"; then
-  __myzs_append_path "$NODE_LTS_PATH"
-fi
+__myzs_append_path "$NODE_LTS_PATH"
 
 if __myzs_is_command_exist "yarn"; then
   export CYARN_BIN="$(yarn global bin)"
