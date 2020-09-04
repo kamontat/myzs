23d4c979511f2a9a15ad6a0a1a3d827f6aa49190
Kamontat Chantrachirathumrong <developer@kamontat.net>
2020-09-04 21:45:47 +0700

    add more docker alias

--- src/alias/docker.sh
+++ src/alias/docker.sh
@@ -2,6 +2,20 @@
 
 __myzs_initial "$0"
 
+if __myzs_is_command_exist "docker"; then
+  __myzs_alias "dps" "docker ps"
+  __myzs_alias "dlog" "docker logs"
+
+  __myzs_alias "drm" "docker rm"
+  __myzs_alias "drm-all" "docker rm $(docker ps -aq)"
+
+  __myzs_alias "di" "docker images"
+  __myzs_alias "drmi" "docker rmi"
+  __myzs_alias "drmi-all" "docker rmi $(docker images -aq)"
+
+  __myzs_alias "dhi" "docker history"
+fi
+
 if __myzs_is_command_exist "docker-compose"; then
   __myzs_alias "dcb" "docker-compose build"
   __myzs_alias "dce" "docker-compose exec"
