f3543716f25598ea54b5f00687a68365813436f5
Kamontat Chantrachirathumrong <developer@kamontat.net>
2020-09-15 13:44:02 +0700

    feat(core): move to plugins

--- src/app/android.sh
+++
@@ -1,8 +0,0 @@
-# shellcheck disable=SC1090,SC2148
-
-_myzs:internal:module:initial "$0"
-
-if _myzs:internal:checker:folder-exist "$HOME/Library/Android/sdk"; then
-  export ANDROID_HOME="$HOME/Library/Android/sdk"                        # android home
-  export PATH="${PATH}:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools" # android path
-fi
