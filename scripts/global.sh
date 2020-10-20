400b19ae52f529f2dd4b7d4be3a1b64d0b6c77b2
Kamontat Chantrachirathumrong <developer@kamontat.net>
2020-10-20 13:27:15 +0700

    feat(script): finish global installation script

--- scripts/global.sh
+++ scripts/global.sh
@@ -10,8 +10,9 @@
 
 #/ -----------------------------------
 #/ Custom:       This allow user custom how it work via environment variable
-#/               1. $VERSION to specify version of the project
+#/               1. $MZ_VERSION to specify version of the project
 #/               2. $SHELL to specify shell file to link
+#/               3. $MZ_PROTOCOL to specify git clone with https or ssh
 #/ Steps:        1. clone the project
 #/               2. link the file
 #/ ------------------------------------------
@@ -22,123 +23,109 @@
 #/ ------------------------------------------
 #// Version:      1.0.0 -- create new script
 
-# move to script directory
-
-# Option 1
-cd "$(dirname "$0")" || exit 1
-
-# Option 2
-# cd "$(dirname "$(realpath "$0")")" || exit 1
-
-# Option 3
-# handle symlink
-# real="$0"
-# [ -h "$real" ] && real="$(readlink "$real")"
-# cd "$(dirname "$real")" || exit 1
-
-help() {
-  grep "^#/ " "global.sh" | sed 's/#\/ //g'
+start_cmd() {
+  if [[ $MZ_DRYRUN == true ]]; then
+    echo "[DBG] $*"
+  else
+    "$@"
+  fi
 }
 
-# [[ "$1" == "--help" ]] ||
-#   [[ "$1" == "-h" ]] ||
-#   [[ "$1" == "-?" ]] ||
-#   [[ "$1" == "help" ]] ||
-#   [[ "$1" == "h" ]] ||
-#   [[ "$1" == "?" ]] &&
-#   help && exit 0
+ask() {
+  export ANSWER
 
-version() {
-  grep "^#// " "global.sh" | sed 's/#\/\/ //g'
+  local title="$1" msg="$2" def="$3"
+  printf "%10s: %-25s [%-10s] -> " "$title" "$msg" "$def"
+  read -r ANSWER
+
+  test -z "$ANSWER" && ANSWER="$def"
 }
 
-# [[ "$1" == "--version" ]] ||
-#   [[ "$1" == "-v" ]] ||
-#   [[ "$1" == "version" ]] ||
-#   [[ "$1" == "v" ]] &&
-#   version && exit 0
+ask_choice() {
+  export ANSWER
 
-# @helper
-throw() {
-	printf '%s\n' "$1" >&2 && is_integer "$2" && exit "$2"
-	return 0
+  local title="$1" msg="$2" def="$3"
+  shift 3
+  local choices=("$@")
+  printf "%10s: %-25s [%-10s] -> " "$title" "$msg" "$def"
+  read -r ANSWER
+
+  test -z "$ANSWER" && ANSWER="$def"
+
+  if [[ "${choices[*]}" =~ $ANSWER ]]; then
+    return 0
+  else
+    echo "'$ANSWER' is not listed in choices [${choices[*]}]" >&2
+    exit 1
+  fi
 }
 
-# @helper
-throw_if_empty() {
-	local text="$1"
-	test -z "$text" && throw "$2" "$3"
-	return 0
+steps() {
+  local number="$curr_step"
+  local total="$total_steps"
+  local name="$1"
+
+  echo "[$number/$total] $name"
 }
 
-# @option
-require_argument() {
-	throw_if_empty "$LONG_OPTVAL" "'$LONG_OPTARG' require argument" 9
+next_section() {
+  echo
+  echo '=============================================='
+  echo
 }
 
-# @option
-no_argument() {
-	[[ -n $LONG_OPTVAL ]] && ! [[ $LONG_OPTVAL =~ "-" ]] && throw "$LONG_OPTARG don't have argument" 9
-	OPTIND=$((OPTIND - 1))
-}
+ask "Optional" "enter location" "${MZ_LOCATION:-$HOME/.myzs}"
+MZ_LOCATION="$ANSWER"
 
-# @syscall
-set_key_value_long_option() {
-	if [[ $OPTARG =~ "=" ]]; then
-		LONG_OPTVAL="${OPTARG#*=}"
-		LONG_OPTARG="${OPTARG%%=$LONG_OPTVAL}"
-	else
-		LONG_OPTARG="$OPTARG"
-		LONG_OPTVAL="$1"
-		OPTIND=$((OPTIND + 1))
-	fi
-}
+ask "Optional" "enter specify version" "${MZ_VERSION:-master}"
+MZ_VERSION="$ANSWER"
 
-load_option() {
-	while getopts 'Hh?Vv-:' flag; do
-		case "${flag}" in
-		H) help && exit 0 ;;
-		h) help && exit 0 ;;
-		\\?) help && exit 0 ;;
-		V) version && exit 0 ;;
-		v) version && exit 0 ;;
-		-)
-			export LONG_OPTARG
-			export LONG_OPTVAL
-			NEXT="${!OPTIND}"
-			set_key_value_long_option "$NEXT"
-			case "${OPTARG}" in
-			help)
-				no_argument
-				help
-				exit 0
-				;;
-			version)
-				no_argument
-				version
-				exit 0
-				;;
-			*)
-				# because optspec is assigned by 'getopts' command
-				# shellcheck disable=SC2154
-				if [ "$OPTERR" == 1 ] && [ "${optspec:0:1}" != ":" ]; then
-					echo "Unexpected option '$LONG_OPTARG', run --help for more information" >&2
-					exit 9
-				fi
-				;;
-			esac
-			;;
-		?)
-			echo "Unexpected option, run --help for more information" >&2
-			exit 10
-			;;
-		*)
-			echo "Unexpected option $flag, run --help for more information" >&2
-			exit 10
-			;;
-		esac
-	done
-}
+SHELL_NAME="$(basename "$SHELL")"
+ask_choice "Optional" "enter specify shell" "${SHELL_NAME:-zsh}" "zsh" "bash"
+SHELL_NAME="$ANSWER"
+SHELLRC=".${SHELL_NAME}rc"
 
-# load_option "$@"
+ask_choice "Optional" "enter clone protocol" "${MZ_PROTOCOL:-ssh}" "https" "ssh"
+MZ_PROTOCOL="$ANSWER"
 
+ask_choice "Optional" "is dryrun" "${MZ_DRYRUN:-false}" "true" "false"
+MZ_DRYRUN="$ANSWER"
+
+next_section
+
+RC_PATH="$HOME/${SHELLRC}"
+BACKUP_SUFFIX="-$(date +%s).backup"
+
+total_steps="3"
+curr_step="1"
+test -d "$MZ_LOCATION" && ((total_steps++))
+test -f "$RC_PATH" && ((total_steps++))
+
+steps "Installing MYZS version ${MZ_VERSION} on ${SHELL_NAME}"
+((curr_step++))
+
+if test -d "$MZ_LOCATION"; then
+  steps "Backup existing myzs directory"
+  ((curr_step++))
+
+  start_cmd mv -r "$MZ_LOCATION" "${MZ_LOCATION}${BACKUP_SUFFIX}"
+fi
+
+steps "Cloning myzs project to local machine"
+((curr_step++))
+
+git_url="git@github.com:kamontat/myzs.git"
+[[ "$MZ_PROTOCOL" == "https" ]] && git_url="https://github.com/kamontat/myzs.git"
+start_cmd git clone --recurse-submodules --branch "$MZ_VERSION" "$git_url" "$HOME/.myzs"
+
+if test -f "$RC_PATH"; then
+  steps "Backup existing .zshrc file"
+  ((curr_step++))
+
+  start_cmd mv -r "$RC_PATH" "${RC_PATH}${BACKUP_SUFFIX}"
+fi
+
+steps "Links zshrc file with root"
+((curr_step++))
+
+start_cmd ln -s "${MZ_LOCATION}/${SHELLRC}" "${RC_PATH}"
