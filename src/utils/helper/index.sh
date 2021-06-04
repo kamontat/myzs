895714414b149fa22d00548dad8c4647b8e3c235
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-06-05 00:12:33 +0700

    perf(util): enable setting v2 as default, since performance issues is fixed

--- src/utils/helper/index.sh
+++ src/utils/helper/index.sh
@@ -1,9 +1,11 @@
 # shellcheck disable=SC1090,SC1091,SC2148
 
-source "${__MYZS__HLP}/setting.sh" # load setting checker
-source "${__MYZS__HLP}/checker.sh" # load checker helper
-source "${__MYZS__HLP}/logger.sh"  # load logger helper
-source "${__MYZS__HLP}/common.sh"  # load all common function
+source "${__MYZS__HLP}/database.sh" # load database apis
+# source "${__MYZS__HLP}/setting.sh"  # load setting checker
+source "${__MYZS__HLP}/setting-v2.sh" # load setting checker v2
+source "${__MYZS__HLP}/checker.sh"    # load checker helper
+source "${__MYZS__HLP}/logger.sh"     # load logger helper
+source "${__MYZS__HLP}/common.sh"     # load all common function
 
 _myzs:internal:load "helper/metric" "${__MYZS__HLP}/metric.sh"       # [extra] load metric
 _myzs:internal:load "helper/module" "${__MYZS__HLP}/module.sh"       # [extra] load module
