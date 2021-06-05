a6f6cee15bb27611ef9bd8f12bc6df1ace004825
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-06-05 17:03:35 +0700

    chore(app): add group function template

--- src/app/group.sh
+++ src/app/group.sh
@@ -4,9 +4,20 @@ _myzs:internal:module:initial "$0"
 
 export __MYZS__GROUP_PREFIX="__MYZS_GROUPS__"
 
-_myzs:internal:group:get() {
-  echo ""
+# load group data from name
+_myzs:internal:group:loader() {
+  local group_name="$1"
+
+  echo "$group_name"
 }
 
-# TODO: Implement group module loading with mg | myzs-group command
-# echo "grouping"
+# create new group with data
+_myzs:internal:group:creator() {
+  local group_name="$1" elements=()
+  shift
+  elements=("$@")
+
+  echo "$group_name: ${elements[*]}"
+}
+
+_myzs:internal:group:creator "dev" "myzs-plugins/editor#app/vscode.sh" "myzs-plugins/git#alias/git.sh"
