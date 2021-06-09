5b25e3796219657e50826aaf124589baf21efab1
Kamontat Chantrachirathumrong <kamontat.chantrachirathumrong@agoda.com>
2021-06-09 17:41:59 +0700

    myzs-load will auto fetch modules in given plugins automatically

--- src/utils/plugin-v2.sh
+++ src/utils/plugin-v2.sh
@@ -9,13 +9,14 @@ export __MYZS__PLUGIN_KEY="plugin"
 # $2 => function: cmd(plugin name, plugin version)
 _myzs:internal:plugin:name-deserialize() {
   local input="$1" cmd="$2"
-  local plugin_name="${input%%#*}"
-  local plugin_version="${input##*#}"
-
   shift 2
 
-  [[ "$plugin_name" == "$plugin_version" ]] && plugin_version="main"
-  $cmd "$plugin_name" "$plugin_version" "$@"
+  local plugin_name="${input%%\#*}"
+  local plugin_version="${input##*\#}"
+
+  # try to get version from database, if not exist fallback to main branch
+  [[ "$plugin_name" == "$plugin_version" ]] && plugin_version="$(_myzs:internal:db:getter:string "$__MYZS__PLUGIN_KEY" "$plugin_name" "main")"
+  $cmd "$plugin_name" "$plugin_version" "$input" "$@"
 }
 
 # convert plugin name and version to plugin key
@@ -37,12 +38,16 @@ _myzs:internal:plugin:name-serialize() {
 }
 
 # show whether plugin is loaded and completed
-_myzs:private:plugin:is-done() {
+_myzs:private:plugin:is() {
   local plugin_name="$1" plugin_version="$2"
-  _myzs:internal:db:checker:string "$__MYZS__PLUGIN_KEY" "$plugin_name-status" "pass" "skip"
+  _myzs:internal:db:checker:string "$__MYZS__PLUGIN_KEY" "$plugin_name-status" "$@"
 }
 
-# _myzs:private:plugin:saved $name $version (pass|fail|skip|unknown)
+# _myzs:private:plugin:saved $name $version (unknown|fail|clone|load)
+#   1. unknown - saving with empty status (dev needs to fix)
+#   2. fail    - cannot doing something (user needs to see more information on log file)
+#   3. clone   - plugin is cloned completely but didn't load module to memory yet
+#   4. load    - plugin module is added to memory
 _myzs:private:plugin:saved() {
   local plugin_name="$1" plugin_version="$2" plugin_status="${3:-unknown}"
 
@@ -53,12 +58,12 @@ _myzs:private:plugin:saved() {
 
 # TODO: add support metrics when we load plugins
 _myzs:internal:plugin:install() {
-  local plugin_name="$1" plugin_version="$2" plugin_status="pass"
+  local plugin_name="$1" plugin_version="$2" plugin_status="clone"
   local plugin_path="${__MYZS__PLG}/$plugin_name"
   local plugin_repo="git@github.com:$plugin_name.git" # TODO: change this to configable data, default git clone using ssh
 
   # skip install when it initial on current terminal session
-  if _myzs:private:plugin:is-done "$plugin_name" "$plugin_version"; then
+  if _myzs:private:plugin:is "$plugin_name" "$plugin_version" "clone" "load"; then
     return 0
   fi
 
@@ -74,7 +79,6 @@ _myzs:internal:plugin:install() {
     fi
   else
     _myzs:internal:log:info "plugin is exist at $plugin_path, skip update"
-    plugin_status="skip"
   fi
 
   # validate myzs.init file
@@ -117,6 +121,47 @@ _myzs:internal:plugin:upgrade() {
 
   # finish upgrading plugin repository
   return 0
+}
+
+# load plugin module to memory as skipped module
+# this quite heavy compute, it might take up to 500ms depend on modules in given plugin
+_myzs:private:plugin:load() {
+  local plugin_name="$1" plugin_version="$2" module_type module_name module_key
+  local plugin_supported_list=("app" "alias" "settings" "utils")
+
+  # skip if it already loaded
+  if _myzs:private:plugin:is "$plugin_name" "$plugin_version" "load"; then
+    return 0
+  fi
+
+  # loading on clone status plugin only
+  if _myzs:private:plugin:is "$plugin_name" "$plugin_version" "clone"; then
+    # search module and initial them as skip
+    for folder in "${plugin_supported_list[@]}"; do
+      plugin_path="$__MYZS__PLG/$plugin_name/$folder"
+      if _myzs:internal:checker:folder-exist "$plugin_path"; then
+        for __plugin_component in "$plugin_path"/*.sh; do
+          filename="$(basename "${__plugin_component}")"
+          dirname="$(basename "$(dirname "${__plugin_component}")")"
+
+          module_type="$plugin_name"
+          module_name="$dirname/$filename"
+          module_key="$(_myzs:internal:module:name-serialize "$module_type" "$module_name")"
+
+          _myzs:internal:module:query _myzs:internal:module:skip "$module_type" "$module_name" "$module_key"
+        done
+      fi
+    done
+
+    return 0
+  fi
+
+  return 1
+}
+
+_myzs:internal:plugin:load() {
+  local plugin="$1"
+  _myzs:internal:plugin:name-deserialize "$plugin" _myzs:private:plugin:load
 }
 
 _myzs:internal:plugin:initial() {
